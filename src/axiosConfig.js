c0be6f88cd7d5e860a157bde186c7e6baf61fcb8
shajiaamjad <shajiaamjad9@gmail.com>
2025-02-02 22:31:41 +0500

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000", //backend requests was going to 3000 by default
+    baseURL: "https://assignment3-backend-mu.vercel.app", //backend requests was going to 3000 by default
 });
 
 export default api;
